6f8411be341b818d3dc5966cad68702b60870534
Daniel Kerstan <danielkerstan@bren.ucsb.edu>
2022-04-17 23:08:36 -0700

    updated

--- render.R
+++ render.R
@@ -1,2 +1,2 @@
-Sys.setenv(RSTUDIO_PANDOC="/usr/local/bin/pandoc")
+Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/quarto/bin")
 rmarkdown::render("/Users/danielkerstan/Desktop/rain/index.Rmd")
